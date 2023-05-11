6002ae1df8873aa9fb27ce0024377c37941e7279
Apip Kurniawan <davitaja92@gmail.com>
2023-05-11 20:44:44 +0700

    add layout user

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\DomPDF\ServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
     ],
 
@@ -193,6 +194,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // ...
+        'PDF'=>Barryvdh\DomPDF\Facade::class,
     ])->toArray(),
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/user', [UserController::class, 'index'])->name('user');
+Route::get('/user/edit/{id}',[UserController::class, 'index'])->name('user.edit');
+Route::get('/user/hapus/{id}' , 'userController@destroy' );
